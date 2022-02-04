304731b792f63f8483dbfc6cf9b22163f5c544a5
Leon Brandhoff <leon.brandhoff@uni-koeln.de>
2022-02-04 16:44:25 +0100

    added new bam comparison script

---
+++ bamqc_v2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BAM1=$1
+BAM2=$2
+MEM=$3
+PICARDOUTP_DIR=$4
+
+# check picard in PATH
+if [[ $(type -P "picard" | wc -l) == 0 ]]; then
+    echo PLEASE ADD PICARD TO PATH VARIABLE
+    return
+    exit 1
+fi
+
+# check md5sums
+if [[ $(md5sum "$BAM1" | cut -f 1 -d " ") == $(md5sum "$BAM2" | cut -f 1 -d " ") ]]; then
+    echo FILES ARE EQUAL
+else  # if unequal, run picard comparesams
+    echo FILES ARE NOT EQUAL, RUNNING PICARD COMPARE BAMS
+    echo
+    echo picard -Xmx"${MEM}" CompareSAMs "$BAM1" "$BAM2" -O "$PICARDOUTP_DIR"
+fi
